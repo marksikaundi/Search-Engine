69a09a1943fe21cb1171d2ad05009e50ba1ffadb
marksikaundi <emmanuelmark117@gmail.com>
2023-05-16 00:36:22 +0200

    body done: all done

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -3,14 +3,26 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
+import Image from "next/image";
 
 export default function HomeSearch() {
   const router = useRouter();
   const [input, setInput] = React.useState("");
+  const [randomSearchLoading, setRandomSearchLoading] = React.useState(false);
   function handleSubmit(e) {
     e.preventDefault();
     if (!input.trim()) return;
     router.push(`/search/web?searchTerm=${input}`);
+  }
+
+  async function randomSearch() {
+    setRandomSearchLoading(true);
+    const response = await fetch("https://random-word-api.herokuapp.com/word")
+      .then((res) => res.json())
+      .then((data) => data[0]);
+    if (!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
   }
   return (
     <>
@@ -29,8 +41,19 @@ export default function HomeSearch() {
       </form>
 
       <div className="flex flex-col space-y-2 justify-center sm:flex-row mt-8 sm:space-y-0 sm:space-x-4">
-        <button className="btn">Google Search</button>
-        <button className="btn"> I&apos;m Feeling Lucky</button>
+        <button onClick={handleSubmit} className="btn ">
+          Google Search
+        </button>
+        <button disabled={randomSearchLoading} onClick={randomSearch} className="btn flex items-center justify-center disabled:opacity-80">
+          {randomSearchLoading ? (
+            <Image 
+            src="loading.svg" 
+            alt="Loading" 
+            className="h-6 text-center" />
+          ) : (
+            "I am Feeling Lucky"
+          )}
+        </button>
       </div>
     </>
   );
