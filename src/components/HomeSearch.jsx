2035e19ea517b82e66b23f8646babf00b852e6e3
marksikaundi <emmanuelmark117@gmail.com>
2023-05-15 23:47:26 +0200

    adding functionalities

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 
 export default function HomeSearch() {
+  const router = useRouter();
+  const [input, setInput] = React.useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  }
   return (
     <>
-      <form className="flex w-full mt-5 mx-auto max-w-[90%]  hover:shadow-md focus-within:shadow-md transition-shadow border border-gray-200 px-5 py-3 rounded-full sm:max-w-xl lg:max-w-2xl">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full mt-5 mx-auto max-w-[90%]  hover:shadow-md focus-within:shadow-md transition-shadow border border-gray-200 px-5 py-3 rounded-full sm:max-w-xl lg:max-w-2xl"
+      >
         <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
-        <input type="text" className="flex-grow focus:outline-none" />
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          className="flex-grow focus:outline-none"
+        />
         <BsFillMicFill className="text-xl text-gray-500" />
       </form>
 
