94d0bb0ca578f2fa060a184fb6854cb62bf70d3e
marksikaundi <emmanuelmark117@gmail.com>
2023-05-15 23:05:31 +0200

    styling homescreen component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -5,7 +5,7 @@ import { BsFillMicFill } from 'react-icons/bs'
 export default function HomeSearch() {
   return (
     <>
-      <form className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full'>
+      <form className='flex w-full mt-5 mx-auto max-w-[90%]  hover:shadow-md focus-within:shadow-md transition-shadow border border-gray-200 px-5 py-3 rounded-full sm:max-w-xl lg:max-w-2xl'>
         <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
         <input type='text' className='flex-grow focus:outline-none' />
         <BsFillMicFill className='text-xl text-gray-500' />
