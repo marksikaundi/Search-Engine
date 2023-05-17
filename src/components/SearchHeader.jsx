30bc743314ee6fef9ec3b4c7bae21f0f044109ad
marksikaundi <emmanuelmark117@gmail.com>
2023-05-17 23:17:10 +0200

    updated search header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -4,11 +4,12 @@ import React from "react";
 import SearchBox from "./SearchBox";
 import { RiSettings3Line } from "react-icons/ri";
 import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
   return (
-    <header>
-      <div className="">
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center justify-between">
         <Link href={"/"}>
           <Image
             src="https://www.freepnglogos.com/uploads/google-logo-png-hd-11.png"
@@ -17,15 +18,18 @@ export default function SearchHeader() {
             height={40}
           />
         </Link>
-        <div className="">
+        <div className="flex-1">
           <SearchBox />
         </div>
-        <div className="">
+        <div className="hidden md:inline-flex space-x-2">
           <RiSettings3Line className="header-icon" />
           <TbGridDots className="header-icon" />
         </div>
-        <button>Sign in</button>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2">
+          Sign in
+        </button>
       </div>
+      <SearchHeaderOptions />
     </header>
   );
 }
