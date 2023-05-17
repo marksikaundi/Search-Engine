d1782039f915dfe568230f1576943457f990bda1
marksikaundi <emmanuelmark117@gmail.com>
2023-05-17 22:51:14 +0200

    working with components

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function SearchHeader() {
   return (
-    <div>
-      
-    </div>
-  )
+    <header>
+      <div className="">
+        <Link href={"/"}>
+          <Image
+            src="https://www.freepnglogos.com/uploads/google-logo-png-hd-11.png"
+            alt="logo"
+            width={120}
+            height={40}
+          />
+        </Link>
+        <div className="">
+
+        </div>
+      </div>
+    </header>
+  );
 }
