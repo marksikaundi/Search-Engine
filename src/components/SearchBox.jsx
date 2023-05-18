2b6348dc656629db6f6e88ab97cd0b8dae1123cd
marksikaundi <emmanuelmark117@gmail.com>
2023-05-18 23:07:48 +0200

    continued with search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { RxCross2 } from "react-icons/rx"
 
 export default function SearchBox() {
   return (
-    <div>
-      Search Box
-    </div>
+    <form>
+    <input type="text" />
+    <RxCross2 />
+    </form>
   )
 }
