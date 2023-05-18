e57ff8be0eab9885b85026ee81731b6f7017a838
marksikaundi <emmanuelmark117@gmail.com>
2023-05-18 23:29:57 +0200

    adding functionalitis to the app

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 import { RxCross2 } from "react-icons/rx";
 
 export default function SearchBox() {
+  const searchParams = useSearchParams();
   return (
     <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
       <input type="text" className="w-full focus:outline-none" />
