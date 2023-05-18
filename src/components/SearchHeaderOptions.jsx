dfee0b4c6efeb1f043cfdec9ddabb4f669e04253
marksikaundi <emmanuelmark117@gmail.com>
2023-05-19 00:18:08 +0200

    search option

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function SearchHeaderOptions() {
   return (
     <div>
-      SearchHeaderOptions
+      <div className="">
+
+      </div>
     </div>
   )
 }
