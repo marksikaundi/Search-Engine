9aff2cbe0c1a3519dbf41173e574257c8a2a950c
marksikaundi <emmanuelmark117@gmail.com>
2023-05-18 23:25:33 +0200

    search input completed

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,8 +5,8 @@ import { RxCross2 } from "react-icons/rx";
 
 export default function SearchBox() {
   return (
-    <form className="flex">
-      <input type="text" />
+    <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
+      <input type="text" className="w-full focus:outline-none" />
       <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" />
       <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 order-l-2 border-gray-300 mr-3" />
       <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" />
