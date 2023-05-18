3b25cb2a7dec98b1db2d5ea772d089f2377a236c
marksikaundi <emmanuelmark117@gmail.com>
2023-05-19 00:07:28 +0200

    search box done

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,19 +1,29 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import React from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 import { RxCross2 } from "react-icons/rx";
 
+
 export default function SearchBox() {
   const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
+  const router = useRouter();
+  const [term, setTerm] = useState(searchTerm || "");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!term.trim) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  }
+
   return (
-    <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
-      <input type="text" className="w-full focus:outline-none" />
-      <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" />
+    <form onSubmit={handleSubmit} className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
+      <input type="text" onChange={(e) => setTerm(e.target.value)} className="w-full focus:outline-none" value={term} />
+      <RxCross2 onClick={() => setTerm("")} className="text-2xl text-gray-500 cursor-pointer sm:mr-2" />
       <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 order-l-2 border-gray-300 mr-3" />
-      <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" />
+      <AiOutlineSearch onClick={handleSubmit} className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" />
     </form>
   );
 }
