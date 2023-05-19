1bfe9d248c21e6b3b9f90e41f3c74d44ee24a54e
marksikaundi <emmanuelmark117@gmail.com>
2023-05-20 00:13:52 +0200

    complete search header option

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,21 +1,39 @@
-"use client"
+"use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineCamera, AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchHeaderOptions() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
   const router = useRouter();
-fuction selectTab(tab) {
-  routor.push(`/search/${tab === "Images" ? "image" : "web" }`)
-}
+  function selectTab(tab) {
+    router.push(
+      `/search/${tab === "Images" ? "image" : "web"}?searchTerm=${searchTerm}`
+    );
+  }
 
   return (
-    <div>
-      <div onClick={() => selectTab("All")} className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === "/search/web" && "!text-blue-600 !border-blue-600"}`}>
+    <div className="flex space-x-2 border-b w-full justify-center select-none lg:justify-start lg:pl-52 text-gray-700 text-sm">
+      <div
+        onClick={() => selectTab("All")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
+          pathname === "/search/all" && "!text-blue-600 !border-blue-600"
+        }`}
+      >
         <AiOutlineSearch className="text-md" />
         <p>All</p>
+      </div>
+      <div
+        onClick={() => selectTab("Images")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
+          pathname === "/search/image" && "!text-blue-600 !border-blue-600"
+        }`}
+      >
+        <AiOutlineCamera className="text-md" />
+        <p>Images</p>
       </div>
     </div>
   );
