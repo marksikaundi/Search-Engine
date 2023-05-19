ed41830721f6631e5ca50f3b7815f1bde9d649a0
marksikaundi <emmanuelmark117@gmail.com>
2023-05-19 23:37:21 +0200

    search header option

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchHeaderOptions() {
   return (
     <div>
-      <div className="">
-
+      <div className="flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2">
+        <AiOutlineSearch className="text-md" />
+        <p>All</p>
       </div>
     </div>
-  )
+  );
 }
