846a1f87314f7d5fe65ed0fb917584bae4478a00
marksikaundi <emmanuelmark117@gmail.com>
2023-05-20 23:02:10 +0200

    removed bug

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -6,22 +6,10 @@ export default async function WebSearchPage(searchParams) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
-  // if (!response.ok) {
-  //   throw new Error("Something went wrong try again later");
-  // }
-
+  
   const data = await response.json();
 
-  if (!results) {
-    return (
-      <div className="flex flex-col justify-center items-center pt-10">
-        <h1 className="text-3xl pb-4"> No results found</h1>
-        <p className="text-lg">Try search something else
-          <Link href="/" className="text-blue-500>Home">Home</Link>
-        </p>
-      </div>
-    );
-  }
+  
 
   const results = data.items;
 
