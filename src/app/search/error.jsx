8e29a4b3370d472815ae230e477af136faa5dd89
marksikaundi <emmanuelmark117@gmail.com>
2023-05-20 14:15:45 +0200

    not completed

--- src/app/search/error.jsx
+++ src/app/search/error.jsx
@@ -6,9 +6,9 @@ export default function Error({error, reset}) {
         console.log(error)
     })    
   return (
-    <div>
-      <h1>Something went wrong</h1>
-      <button onClick={() => reset()}>Try again</button>    
+    <div className='flex flex-col justify-center items-center pt-10'>
+      <h1 className='text-3xl md-4'>Something went wrong</h1>
+      <button className='text-blue-500' onClick={() => reset()}>Try again</button>    
     </div>
   )
 }
