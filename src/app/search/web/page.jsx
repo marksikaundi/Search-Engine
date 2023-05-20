03de3360d48b313dd94b63af630ac2a4cefa0140
marksikaundi <emmanuelmark117@gmail.com>
2023-05-20 23:43:37 +0200

    fixing error

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -6,12 +6,24 @@ export default async function WebSearchPage(searchParams) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if(!response.ok) {
+    throw new Error('Something went wrong')
+  }
   
+
   const data = await response.json();
 
-  
-
   const results = data.items;
+
+  if(!results) {
+    return <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">No result found</h1>
+      <p className="text-lg">
+        Try searching for something else or go back to{" "}
+        <Link href="/" className="text-blue-500">Home</Link>
+      </p>
+      </div>
+  }
 
   return (
     <>
