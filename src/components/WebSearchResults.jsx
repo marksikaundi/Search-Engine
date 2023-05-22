0ce13e9be07818ae34307c6de0d424047e810db3
marksikaundi <emmanuelmark117@gmail.com>
2023-05-22 22:28:00 +0200

    updates

--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function WebSearchResults({results}) {
   return (
-    <div>
-      <p>
+    <div className='w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-[52]'>
+      <p className='text-gray-600 text-sm mb-5 mt-3'>
         About
         {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds) 
       </p>
