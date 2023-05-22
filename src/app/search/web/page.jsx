139bbb3e6638f02f28f08cb79538b16804a00fa6
marksikaundi <emmanuelmark117@gmail.com>
2023-05-22 23:05:26 +0200

    search results done

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage(searchParams) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
