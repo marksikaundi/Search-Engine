77efe1b731d493e19012717b11877db20b598bfa
marksikaundi <emmanuelmark117@gmail.com>
2023-05-24 23:31:12 +0200

    Pagenation component done

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -4,7 +4,7 @@ import PagenationButton from "./PagenationButton";
 
 export default function ImageSearchResults({ results }) {
   return (
-    <div className="pb-24 mt-4">
+    <div className="sm:pb-24 pb-40 mt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results.items.map((result) => (
             <div key={result.link} className="mb-8">
