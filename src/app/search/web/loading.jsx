04ae0f2738918e584d473180a742e91df9eb237c
marksikaundi <emmanuelmark117@gmail.com>
2023-05-25 22:32:35 +0200

    adding loading effects

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <>
+      <div className="">
+        <div className="h-2.5 w-48 bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-3.5 w-[360px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="w-[560px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="w-[530px] bg-gray-200 rounded-full mb-2.5"></div>
+      </div>
+    </>
+  );
+}
