4ca1723e0c206a8ff5db9226a623663628e6786c
marksikaundi <emmanuelmark117@gmail.com>
2023-05-25 23:01:59 +0200

    completed loading effect

--- src/app/search/image/loading.jsx
+++ src/app/search/image/loading.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function loading() {
   return (
-    <div>
-      loading....
+    <div className="pt-10 mx-2 lg:pl-52 max-w-6xl flex sm:space-x-4 flex-col sm:flex-row pb-42">
+      <div className="animate-pulse">
+        <div className="h-45 w-48 mb-4 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-44 mb-2.5 bg-gray-200 rounded-md"></div>
+      </div>
+      <div className="animate-pulse">
+        <div className="h-45 w-48 mb-4 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-44 mb-2.5 bg-gray-200 rounded-md"></div>
+      </div>
+      <div className="animate-pulse">
+        <div className="h-45 w-48 mb-4 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-44 mb-2.5 bg-gray-200 rounded-md"></div>
+      </div>
+      <div className="animate-pulse">
+        <div className="h-45 w-48 mb-4 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-44 mb-2.5 bg-gray-200 rounded-md"></div>
+      </div>
     </div>
-  )
+  );
 }
