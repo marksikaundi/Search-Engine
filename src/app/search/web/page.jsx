8f36a262bb1f155c283707230f2dde48cc3b733e
marksikaundi <emmanuelmark117@gmail.com>
2023-05-26 21:36:05 +0200

    loading time out

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,37 +1,36 @@
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage(searchParams) {
   const startIndex = +searchParams.start || "1";
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
   );
 
-  if(!response.ok) {
-    throw new Error('Something went wrong')
+  if (!response.ok) {
+    throw new Error("Something went wrong");
   }
-  
 
   const data = await response.json();
 
   const results = data.items;
 
-  if(!results) {
-    return <div className="flex flex-col justify-center items-center pt-10">
-      <h1 className="text-3xl mb-4">No result found</h1>
-      <p className="text-lg">
-        Try searching for something else or go back to{" "}
-        <Link href="/" className="text-blue-500">Home</Link>
-      </p>
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No result found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
       </div>
+    );
   }
 
-  return (
-    <>
-      {results && <WebSearchResults results={data} />}
-    </>
-  );
+  return <>{results && <WebSearchResults results={data} />}</>;
 }
